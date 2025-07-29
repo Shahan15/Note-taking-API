92777d14e6fcd1e30144a74c6dae97a65db6393a
shahan15 <k.shahan786@gmail.com>
2025-07-30 00:22:05 +0100

    added fresh file reading from disk + postman response

--- server.js
+++ server.js
@@ -43,17 +43,30 @@ const server = http.createServer((req, res) => {
 
         req.on("end",() => {
             console.log("All Chunks have arrived")
-            const newNoteToAdd = JSON.parse(body);
-            notesToObject.push(newNoteToAdd);
-            console.log("Successfully added new note to notes.json")
 
-            fs.writeFile('notes.json', JSON.stringify(notesToObject), 'utf-8', (err) => {
-                if(err){console.log(`error encountered ${err}`)} else{console.log("Notes added successfully")}
+            const newNoteToAdd = JSON.parse(body);
+            
+            //Read the file fresh every time
+            const notes = fs.readFileSync('notes.json','utf-8');
+            const notesArray = JSON.parse(notes);
+
+            //Push new note
+            notesArray.push(newNoteToAdd)
+
+
+            fs.writeFile('notes.json', JSON.stringify(notesArray), 'utf-8', (err) => {
+                if(err){
+                    console.log(`error encountered ${err}`)
+                } 
+                else{
+                    console.log("Notes added successfully")
+                    res.writeHead(201,{'Content-Type':'application/json'});
+                    res.end(JSON.stringify({ status: "success", note: newNoteToAdd }));
+                }
             })
         });
 
-        res.writeHead(201,{'Content-Type':'application/json'});
-        res.end(JSON.stringify({ status: "success", note: newNoteToAdd }));
+        
     };
 });
 
